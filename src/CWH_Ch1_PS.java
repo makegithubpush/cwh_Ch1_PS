4b7c82d479e2adc35e7c8dce98649d3dacca2ed1
makegithubpush <ts723682@gmail.com>
2024-09-08 20:39:18 +0530

    64th commit

---
+++ src/CWH_Ch1_PS.java
@@ -0,0 +1,46 @@
+
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class CWH_Ch1_PS {
+    public static void main(String[] args) {
+//        Question1
+//        int a = 45;
+//        int b = 34;
+//        int c = 66;
+//        int sum = a + b + c;
+//        System.out.println("The Sum of These Numbers is");
+//        System.out.println(sum);
+
+//        Question2
+//        float Subject1 = 45;
+//        float Subject2 = 95;
+//        float Subject3 = 48;
+//        float cgpa = ((Subject1 + Subject2 + Subject3)/300*10);
+//        System.out.println(cgpa);
+
+//        Question3
+        System.out.println("What is your name");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println("Hello " + name + " Have a good day!");
+
+//        Question4
+//      Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the distance in km");
+//        double a = sc.nextDouble();
+//        double miles = (a*0.621371);
+//        System.out.println("miles");
+//        System.out.println(miles);
+
+//        Question5
+//     Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the integer");
+
+//        boolean b1 = sc.hasNextInt();
+//        System.out.println(b1);
+
+    }
+}
